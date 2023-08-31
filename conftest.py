d45f6a20686f0a5d199137dfa2b0f149011b0755
cherkasskypavel <fenderik09@mail.ru>
2023-08-31 23:35:00 +0300

    Завершенный вариант. selenium_course, section 3, lesson 6, step 9.

--- conftest.py
+++ conftest.py
@@ -1,19 +1,34 @@
 import pytest
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as Chrome_Options
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options as Firefox_Options
 
-
+#   добавляем опции:
+#       - по заданию языка на странице;
+#       - по выбору браузера (только хром или файерфокс, хром по умолчанию)
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
                      help='Choose language')
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help='Choose browser: chrome (by default) or firefox')
 
+#   в условном операторе объявляем разные Options и webdriver для каждого браузера
 @pytest.fixture(scope='function')
 def browser(request):
-    user_language = request.config.getoption('language')
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    print('\nоткрываем хром\n')
-    browser = webdriver.Chrome()
+    browser_name = request.config.getoption('--browser_name')
+    user_language = request.config.getoption('--language')
+    if browser_name == 'chrome':
+        options = Chrome_Options()
+        options.add_experimental_option('prefs', {"intl.accept_languages": user_language})
+        print('\nоткрываем хром\n')
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == 'firefox':
+        options = Firefox_Options()
+        options.set_preference("intl.accept_languages", user_language)
+        print('\nоткрываем файерфокс\n')
+        browser = webdriver.Firefox(options=options)
+    else:
+        raise pytest.UsageError('--browser_name should be chrome or firefox')
     yield browser
-    print('\nзакрываем хром\n')
+    print('\nзакрываем браузер\n')
     browser.close()
--- test_items.py
+++ test_items.py
@@ -1,19 +1,27 @@
+import pytest
 from selenium.webdriver.common.by import By
 import selenium.webdriver.support.expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 import time
-class TestShop():
 
+class TestShop():
+    def presence_of_element(self, browser, css_selector=str):
+        try:
+            elem = WebDriverWait(browser, 5).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, css_selector))
+            )
+            print(f'Текст в элементе: "{elem.text}"')
+            return True
+        except:
+            # print('Элемент отсутствует')
+            return False
     def test_item_page_contains_basket_button(self, browser):
         link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
         browser.get(link)
-        time.sleep(10)
-        basket_button_checking = WebDriverWait(browser, 5).until(
-            EC.visibility_of_element_located((By.CSS_SELECTOR, '#add_to_basket_form'))
-        )
-        child_element_button_check = browser.find_elements(
-            By.CSS_SELECTOR, "#add_to_basket_form button"
-        )
-        assert len(child_element_button_check) > 0
+        # time.sleep(30)
+        basket_button_checking = self.presence_of_element(browser, '#add_to_basket_form button')
+        assert basket_button_checking == True
 
+if __name__ == '__main__':
+    pytest.main()
